861c1742f2af847ba200d5575d55b92051bc60a9
ISTIFANO <elamiriamir7@gmail.com>
2026-07-18 15:33:50 +0100

    add smoke test

---
+++ src/test/java/com/fooddelivery/FoodDeliveryApplicationTests.java
@@ -0,0 +1,13 @@
+package com.fooddelivery;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+
+class FoodDeliveryApplicationTests {
+
+    @Test
+    void applicationClassExists() {
+        assertNotNull(FoodDeliveryApplication.class);
+    }
+}
